c3689a2a1a1c600ecf0df433dcd752df81d3c54e
Mitchell Houston <c3334326@uon.edu.au>
2019-04-05 03:32:58 +0000

    End of week 1 lab of assesment task

--- Encription.c
+++ Encription.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
-int main(void)
+
+ 
+int main()
 {
-    printf("hello, world test \n");
-}
-test
+	char str[]= "aaaaadefghijklmnopqrs", ex;
+	int i, 
+	k=-1;
+	
+	printf("Message %s\n",str);
+	
+	
+	for(i = 0; str[i] != '\0' ; ++i){
+		ex = str[i];
+		
+	    
+	
+		str[i]=ex+k;
+		
+		/*if(ex >= 'a' && ex <= 'z'){
+			ex = ex + k;
+			
+			if(ex > 'z'){
+				ex = ex - 'z' + 'a' ;
+			}
+			
+			str[i] = ex;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){
+			ch = ch + key;
+			
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1;
+			}
+			
+			*/str[i] = ch;
+		}
+	}
+	
+	printf("Encrypted messages: %s", str);
+	
+	return 0;
+}  

7bb30e5cb77c7b280c53f7834b664fb26ebcad39
Mitchell Houston <c3334326@uon.edu.au>
2019-04-06 06:08:04 +0000

    Enription cipher underway

--- Encription.c
+++ Encription.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
 
-
+void message(char str[],int k)
  
 int main()
 {
-	char str[]= "aaaaadefghijklmnopqrs", ex;
-	int i, 
+	char str[]= "ABCDEF", ex;
 	k=-1;
 	
-	printf("Message %s\n",str);
 	
+
+	return 0;
+}	
 	
-	for(i = 0; str[i] != '\0' ; ++i){
+void message(char str[],int k)	
+
+    {
+        int i = 0;
+        
+        for(i=0;str[i] != '\0';i++)
+        {
+            if (str[i] >= 'A' && str[i]<= 'Z'>);
+            {
+                char ex = str[i]-'A' + 26;
+                ex = (ex+k) % 26;
+                str[i] = ex + 'A';
+           }
+           else if (str[i] >= '0' && str[i] <= '9')
+           {
+                char ex = str[i] - '0'+10;
+                ex = (ex +k) %10;
+                str[i]= ex + '0';
+            }
+        }
+    
+    
+	printf("%s\n", str[i]);
+    }
+	/*for(i = 0; str[i] != '\0' ; ++i){
 		ex = str[i];
 		
+		if(ex>65  )
 	    
 	
 		str[i]=ex+k;
@@ -34,11 +60,12 @@ int main()
 				ch = ch - 'Z' + 'A' - 1;
 			}
 			
-			*/str[i] = ch;
-		}
+			str[i] = ch;
+		
 	}
 	
 	printf("Encrypted messages: %s", str);
 	
-	return 0;
+	return 0;*/
+	
 }  
